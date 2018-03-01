1adc3696ba1e1f38e8692a06f288c60c9f298549
Facundo.P <facundo.huira@gmail.com>
2018-03-01 17:00:10 -0300

    segundo commit antes de hacer pull de isa

--- src/application/view/RegisterController.java
+++ src/application/view/RegisterController.java
@@ -175,7 +175,7 @@ public class RegisterController implements Initializable {
         categoriaEmpleadoComboBox.setItems(categoriaEmpleadoRepository.view());
     }
     private void cargarLocalidades() {
-        int idTucuman = 23; //hardcode id tucuman
+        int idTucuman = 24; //hardcode id tucuman
         localidadComboBox.setItems(localidadRepository.view(idTucuman));
     }
 
